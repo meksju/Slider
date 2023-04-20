d6f7e1a6296fde3062e03340f2fa119bb8b31159
meksju <you@example.com>
2023-04-20 22:36:11 +0200

    substituted broken images

--- wargaming/src/App.js
+++ wargaming/src/App.js
@@ -3,12 +3,12 @@ import Slider from "./Components/Slider/Slider";
 
 function App() {
     const slides = [
-        {url: "http://localhost:3000/img/img1.png", title: "germany"},
-        {url: "http://localhost:3000/img/img2.png", title: "ussr"},
-        {url: "http://localhost:3000/img/img3.png", title: "uss"},
-        {url: "http://localhost:3000/img/img4.png", title: "france"},
-        {url: "http://localhost:3000/img/img5.png", title: "united kingdom"},
-        {url: "http://localhost:3000/img/img6.png", title: "czechoslovakia"}
+      {url: "http://localhost:3000/img/img1.png", title: "germany"},
+      {url: "http://localhost:3000/img/img2.png", title: "ussr"},
+      {url: "http://localhost:3000/img/img3.jpg", title: "uss"},
+      {url: "http://localhost:3000/img/img4.jpg", title: "france"},
+      {url: "http://localhost:3000/img/img5.jpg", title: "united kingdom"},
+      {url: "http://localhost:3000/img/img6.jpg", title: "czechoslovakia"}
     ];
 
   return (
